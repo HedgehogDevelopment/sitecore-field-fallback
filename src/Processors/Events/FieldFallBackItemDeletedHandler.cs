aaf75c3b226087b0d32c36bebdf378ee6a600504
Chris Sigmon <csigmon@hhogdev.com>
2016-03-28 18:01:29 -0400

    Create Field FallBack Item Deleted Handler

---
+++ src/Processors/Events/FieldFallBackItemDeletedHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FieldFallback.Processors.Events
+{
+    public class FieldFallBackItemDeletedHandler
+    {
+        public void OnDeleteItem(object sender, EventArgs args)
+        {
+            throw new NotImplementedException("This will get enabled shortly.");
+        }
+    }
+}
