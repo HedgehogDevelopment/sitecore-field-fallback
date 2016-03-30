71ab4fd83aa3d1985b70c1128d26dcc76e459f5c
Chris Sigmon <csigmon@hhogdev.com>
2016-03-30 14:43:06 -0400

    Create Initial DefaultValuesItemProcessor

---
+++ src/Processors/DefaultValuesItemProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FieldFallback.Pipelines.FieldFallbackPipeline;
+using FieldFallback.Processors.Data.Fields;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Data.Templates;
+using Sitecore.Events;
+using Sitecore.SecurityModel;
+
+namespace FieldFallback.Processors
+{
+    public class DefaultValuesItemProcessor : FieldFallbackProcessor
+    {
+        protected override bool IsEnabledForField(Field field)
+        {
+            //This processor is always enabled because it is handled
+            // by create and delete events
+            return true;
+        }
+
+        protected override string GetFallbackValue(FieldFallbackPipelineArgs args)
+        {
+            return null; 
+        }
+    }
+}
