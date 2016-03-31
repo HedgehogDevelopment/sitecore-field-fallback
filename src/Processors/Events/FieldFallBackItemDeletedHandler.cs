87f1346a9c675cffa596e6d66b6cd86bb69d5047
Chris Sigmon <csigmon@hhogdev.com>
2016-03-30 23:02:49 -0400

    Field Fall back Item Deleted Handler Place Holder add comment that the event will delete the item when the template is deleted.

--- src/Processors/Events/FieldFallBackItemDeletedHandler.cs
+++ src/Processors/Events/FieldFallBackItemDeletedHandler.cs
@@ -6,6 +6,7 @@ namespace FieldFallback.Processors.Events
     {
         public void OnDeleteItem(object sender, EventArgs args)
         {
+            //The item will be deleted when the template is deleted
             throw new NotImplementedException("This will get enabled shortly.");
         }
     }
