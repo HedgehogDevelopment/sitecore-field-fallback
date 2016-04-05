107b6087b0be9dd768e53d35418e380848a7ae0e
Chris Sigmon <csigmon@hhogdev.com>
2016-04-05 10:44:14 -0400

    Major refacroting to Created Handler to use background timer, Extension methods, reduce code, and additional config file changes integrations

--- src/Processors/Events/FieldFallBackItemCreatedHandler.cs
+++ src/Processors/Events/FieldFallBackItemCreatedHandler.cs
@@ -1,21 +1,49 @@
 ﻿using System;
+using System.Threading;
 using FieldFallback.Processors.Configuration;
-using Sitecore;
 using Sitecore.Data;
+using Sitecore.Data.Events;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Events;
 using Sitecore.SecurityModel;
+using FieldFallback.Processors.Extensions;
 
 namespace FieldFallback.Processors.Events
 {
+    /// <summary>
+    ///     A field fall back item created handler.
+    ///     This handler functions to create items based off
+    ///     of templates that are created. It is documented
+    ///     throughout. However, one key point is item:created
+    ///     was chosen over item:saved specifically. Also note
+    ///     that there is a background thread that runs on a 
+    ///     delay to create the item to ensure the SaveUI
+    ///     Pipeline has time to process saving the TemplateItem
+    /// </summary>
+
     public class FieldFallBackItemCreatedHandler
     {
+        /// <summary>
+        ///     Raises the item saved event.
+        ///     This event will create an item based off of the
+        ///     template in the same location in the content tree
+        ///     as it is in the template tree.
+        /// </summary>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="args">     Event information to send to registered event handlers. </param>
+
         public void OnItemSaved(object sender, EventArgs args)
         {
-            SitecoreEventArgs eventArgs
-                = args as SitecoreEventArgs;
-
+            // Get the item created event arguments.
+            // We are using the item:Created event over the item:Saved event
+            // The purpose of using the create over the save event is the 
+            // complexity of the SaveUI Pipeline may cause adverse affects
+            // to creating the defaults for the dynamic template creation
+            ItemCreatedEventArgs eventArgs = 
+                Event.ExtractParameter(args, 0) as ItemCreatedEventArgs;
+            
             Assert.IsNotNull(eventArgs, "eventArgs");
 
             if (eventArgs == null)
@@ -23,10 +51,20 @@ namespace FieldFallback.Processors.Events
                 return;
             }
 
-            Item item = eventArgs.Parameters[0] as Item;
+            Item item = eventArgs.Item;
+            Assert.IsNotNull(item, "item");
 
+            // Validate that the item is in the database
+            // that is configured 
+            if (!item.IsInConfigDatabase())
+            {
+                return;
+            }
 
-            if (!isTemplate(item))
+            // Check to see that the item is a template
+            // If it is not a template, we do not need to 
+            // process it.
+            if (!item.IsTemplate())
             {
                 return;
             }
@@ -34,39 +72,65 @@ namespace FieldFallback.Processors.Events
             //If our new item is in our default template location,
             // we are creating a template. Since we are creating a template,
             // we need to create a default item to match it.
-
-            if (item.Paths.FullPath.ToLower().StartsWith(Config.DefaultTemplateLocation.ToLower()))
+            if (item.IsInItemTemplatePath())
             {
-                //Create a new item in the item location based off of the template
-                //that was just created. 
-                using (new SecurityDisabler())
+                // We are creating the new item in a new thread
+                // This allows the Template time create and save
+                // to the database.
+                // It is important that the thread be ran in the
+                // background AND we allow the Thread to sleep
+                // giving time for the current process to complete.
+                Thread createdThread = new Thread(() =>
                 {
-                    //Get the database context
-                    Database context = item.Database;
+                    Thread.CurrentThread.IsBackground = true;
+                    Thread.Sleep(2000);
 
-                    //Get the parent item from the config folder
-                    Item defaultParentItem = context.Items.GetItem(Config.DefaultItemLocation.ToLower());
+                    CreateContentItem(item);
+                });
 
-                    defaultParentItem.Editing.BeginEdit();
-
-                    //Create a new item based off of the template just created
-                    string itemName = item.Name + " Defaults";
-
-                    TemplateItem template = item.Database.GetTemplate(item.ID);
-                    Item newItem = defaultParentItem.Add(itemName, template);
-
-                    defaultParentItem.Editing.EndEdit();
-                }
+                createdThread.Start();
             }
         }
 
-        private bool isTemplate(Item item)
+        /// <summary>
+        ///     Creates content item based off of the template.
+        /// </summary>
+        ///
+        ///
+        /// <param name="item"> 
+        ///     The TemplateItem that was just created. 
+        /// </param>
+
+        private void CreateContentItem(TemplateItem item)
         {
-            if (item.TemplateID.ToString() == TemplateIDs.Template.ToString())
+            //Create a new item in the item location based off of the template
+            //that was just created. 
+            using (new SecurityDisabler())
             {
-                return true;
+                //Get the database context
+                Database context = item.Database;
+                
+                //Get the parent item of the item we need to create
+                Item parentItem = context.Items.GetItem(item.GetParentContentPath());
+
+                if (parentItem == null)
+                {
+                    Assert.IsNull(parentItem, "Fallback Item");
+                }
+
+                parentItem.Editing.BeginEdit();
+
+
+                //Get the template to base the template 
+                TemplateItem template = item.Database.GetTemplate(item.ID);
+
+                string newItemName = item.Name + Config.ContentItemSuffix;
+
+                //Create a new item based off of the template just created
+                parentItem.Add(newItemName, template);
+
+                parentItem.Editing.EndEdit();
             }
-            return false;
         }
     }
-}
+}   
